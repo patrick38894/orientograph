a846a499e780ce4658400dd0afccd86aa5b04716
patrick <snixromero@gmail.com>
2015-03-14 18:07:05 -0700

    fixed typing bugs, still several logical bugs

--- rotation.hs
+++ rotation.hs
@@ -19,10 +19,10 @@ data Permutation = Perm [Int] --permutation
 
 --functions used to generate permutations sigma and theta
 theta :: Graph -> Dart -> Dart
-theta g (v,e) = (g !! e, fromJust $ elemIndex v $ g !! e)
+theta g (v,e) = (e, fromJust $ elemIndex v $ g !! e)
 
 sigma :: Graph -> Dart -> Dart
-sigma g (v,e) = (v, (e + 1) `mod` length $ g !! v)
+sigma g (v,e) = (v, (e + 1) `mod` (length $ g !! v))
 
 --group class for easy construction of groups
 class Group g where
@@ -38,16 +38,22 @@ instance Group Permutation where --permuation group
 	gmul (Perm a) (Perm b) = Perm $ map (\x -> b !! x) a
 	a * b = a `gmul` b
 	inv (Perm a) = Perm $ invBubSort a 0
-		where invBubSort a n = case elemIndex n a of Just x -> x:(invBubSort a n+1)
+		where invBubSort a n = case elemIndex n a of Just x -> x:(invBubSort a $ n+1)
 							     Nothing -> []
 		-- ^ O(n2) is the least of our worries
+
+
+
+--create a permutation from a list of mapping pairs
+pzip :: [(Int, Int)] -> Permutation
+pzip a = Perm $ map snd $ sortBy (\x y -> compare (fst x) (fst y)) a
 
 --construct embedded rotation system from graph g
 
 rotations :: Graph -> (Permutation, Permutation)
 --construct set of all Darts (connected vertex, di-edges)
 --generate permutations for the darts
-rotations g = (Perm $ map sigma b, Perm $ map theta b)
+rotations g = (pzip $ map (sigma g) b, pzip $ map (theta g) b)
 	where b = darts g
 
 
@@ -55,7 +61,7 @@ rotations g = (Perm $ map sigma b, Perm $ map theta b)
 darts :: Graph -> [Dart]
 darts g = darts' g 0
 	where darts' [] n = []
-	      darts' (v:vs) n = zip (repeat n) v ++ darts' vs n+1
+	      darts' (v:vs) n = zip (repeat n) v ++ (darts' vs $ n+1)
 
 
 --to find the genus use Euler formula -> genus = 1 -1/2(|Z(sig)|-|Z(th)|+|Z(sig*th)|)
@@ -71,20 +77,20 @@ orbit (Perm p) i = let orbit' i s = if (p!!i) `elem` s
 --find the genus of graph g
 genus :: Graph -> Int
 genus g = let b = darts g
-	      sz a = length $ partitionWith (orbit a) b
+	      sz a = length $ partitionWith (orbit a) [0.. length b - 1]
 	      rot = rotations g
 	      t = snd rot
 	      s = fst rot
-	      st = nub $ liftM2 (gmul) s t
+	      st = s `gmul` t
 	  in 1 - (sz s - sz t + sz st) `quot` 2
 
 
 
 
 ----------------utility----------------
-partitionWith :: (a -> [a]) -> [a] -> [[a]]
+partitionWith :: (Eq a) => (a -> [a]) -> [a] -> [[a]]
 partitionWith f [] = []
-partitionWith f (x:xs) = (f x):(filter (\a -> not $ a `elem` f x) xs)
+partitionWith f (x:xs) = (f x):(partitionWith f $ filter (\a -> not $ a `elem` f x) xs)
 
 
 
